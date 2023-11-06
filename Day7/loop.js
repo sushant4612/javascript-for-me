f8e2cd3e08030c9feff885be53b5ee0e8311fd57
Sushant <109458485+sushant4612@users.noreply.github.com>
2023-11-06 08:46:54 +0000

    Day7 added

---
+++ Day7/loop.js
@@ -0,0 +1,22 @@
+// loops in javaScript means a block of code that runs number of times which we specifies
+// if you want to run the same code over and over again we use loops
+
+// There are 3 primary loops in javascript
+// for, while, do-while
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// i = 0
+// while(i < 10){
+//     console.log(i);
+//     i++;
+// }
+
+j = 0
+do{
+    console.log(j)
+    j++
+}while(j < 10)
+
