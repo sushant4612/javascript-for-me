1c5d313e78fb53da3ea745429e12d5aee60cc5d4
Sushant <109458485+sushant4612@users.noreply.github.com>
2024-01-18 16:43:42 +0000

    Basic/String file added

---
+++ Basics/Strung.js
@@ -0,0 +1,21 @@
+// String Declaration in javascript
+
+let str1 = "Hello World";
+let str2 = 'Hello World';
+let str3 = new String("Hello world");
+
+// String concatination in js
+
+let greet = "Hello";
+let name = "Sushant";
+
+// console.log(greet + name);
+
+// console.log(`${greet} ${name}`);
+
+// String Methods
+
+let str = "Hello"
+console.log(str[2]);
+console.log(str.charAt(2));
+console.log(str.indexOf('l'));
